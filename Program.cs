c0074721197c4860de69d21d526e510c1b377274
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-01-25 11:19:36 -0800

    write city names to files

--- Program.cs
+++ Program.cs
@@ -14,14 +14,21 @@ internal class Program
             }
             generator.Add(city);
         }
-        Console.WriteLine("Data loaded, saving cache...");
+        Console.WriteLine("Data loaded.");
         Querier.SaveCache();
         Console.WriteLine("Generating city names...");
+        _ = Directory.CreateDirectory("output");
         foreach (string biome in generatorsByBiome.Keys.Order())
         {
+            string fileName = $"output/{biome.Replace("/", ",")}.txt";
             Console.WriteLine($"{biome}:");
+            File.WriteAllText(fileName, "");
             for (int i = 0; i < 10; i++)
-                Console.WriteLine($"\t{generatorsByBiome[biome].RandomStringOfLength(5)}");
+            {
+                string cityName = generatorsByBiome[biome].RandomStringOfLength(min: 5, max: 40);
+                Console.WriteLine($"\t{cityName}");
+                File.AppendAllText(fileName, $"{cityName}\n");
+            }
         }
     }
 }
