68970ab6d772ebd790771c05592d270f04721f78
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-01-24 23:35:19 -0800

    should be working

--- Program.cs
+++ Program.cs
@@ -4,15 +4,21 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        IEnumerable<string> cityNames = File.ReadAllLines("C:/Users/dninemfive/Downloads/geonames-all-cities-with-a-population-1000.csv")
-                                            .Select(x => x.Split(";")[1]);
-        MarkovStringGenerator markov = new(cityNames);
-        Console.WriteLine(markov.MostCommonPairs(10));
-        List<string> names = new();
-        for (int i = 0; i < 10; i++)
-            names.Add($"{markov.RandomStringOfLength(min: 5)}");
-        File.WriteAllLines("city names.txt", names);
-        File.WriteAllText("most common pairs.txt", markov.MostCommonPairs());
-        File.WriteAllText("response.json", await Querier.GetBiomeAsync(34.3, -119));
+        Dictionary<string, MarkovStringGenerator> generatorsByBiome = new();
+        await foreach((string city, string biome) in Querier.GetAllCities())
+        {
+            if(!generatorsByBiome.TryGetValue(biome, out MarkovStringGenerator? generator))
+            {
+                generator = new();
+                generatorsByBiome[biome] = generator;
+            }
+            generator.Add(city);
+        }
+        foreach(string biome in generatorsByBiome.Keys.Order())
+        {
+            Console.WriteLine($"{biome}:");
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine($"\t{generatorsByBiome[biome].RandomStringOfLength(5)}");
+        }
     }
 }
--- Querier.cs
+++ Querier.cs
@@ -11,11 +11,30 @@ namespace citynames;
 public static class Querier
 {
     public static readonly string ArcGisQueryUrl = File.ReadAllText(@"C:\Users\dninemfive\Documents\workspaces\misc\citynames\arcgis query url.txt");
-    private static HttpClient _client = new();
-    public static async IAsyncEnumerable<(string city, double latitude, double longitude)> GetCities()
+    public static readonly string WikidataQueryUrl = File.ReadAllText(@"C:\Users\dninemfive\Documents\workspaces\misc\citynames\wikidata query url.txt");
+    public const string WikidataQueryResultPath = @"C:\Users\dninemfive\Documents\workspaces\misc\citynames\wikidata query result.json";
+    private static readonly HttpClient _client = new();
+    public static async IAsyncEnumerable<(string city, string biome)> GetAllCities()
     {
-        throw new NotImplementedException();
-        yield break;
+        foreach((string city, double latitude, double longitude) in GetCityData())
+        {
+            string? biome = await GetBiomeAsync(latitude, longitude);
+            if (biome is null)
+                continue;
+            yield return (city, biome);
+        }
+    }
+    public static IEnumerable<(string city, double latitude, double longitude)> GetCityData(int threshold = 50000, int limit = 10000)
+    {
+        return JsonSerializer.Deserialize<List<WikidataResultItem>>(File.ReadAllText(WikidataQueryResultPath))!
+                             .Select(x => x.ToData());
+        /* todo: get the proper permissions or whatever to do this in code
+        HttpResponseMessage? response = await _client.GetAsync(WikidataQueryUrl.Replace("{threshold}", $"{threshold}")
+                                                                               .Replace("{limit}", $"{limit}"));
+        if (response is null)
+            yield break;
+        yield return (response.PrettyPrint(), 0, 0);
+        */
     }
     public static async Task<string?> GetBiomeAsync(double latitude, double longitude)
     {
@@ -23,14 +42,34 @@ public static class Querier
                                                                                        .Replace("{x}", $"{longitude}"));
         if (doc is null)
             return null;
-        return doc.RootElement.GetProperty("layers")
-                              .EnumerateArray()
-                              .First()
-                              .GetProperty("features")
-                              .EnumerateArray()
-                              .First()
-                              .GetProperty("attributes")
-                              .GetProperty("BIOME_NAME")
-                              .GetString();
+        try
+        {
+            return doc.RootElement.GetProperty("layers")
+                                  .EnumerateArray()
+                                  .First()
+                                  .GetProperty("features")
+                                  .EnumerateArray()
+                                  .First()
+                                  .GetProperty("attributes")
+                                  .GetProperty("BIOME_NAME")
+                                  .GetString();
+        } catch(Exception e)
+        {
+            Console.WriteLine($"Could not get biome for ({latitude}, {longitude}). {e.GetType().Name}: {e.Message}");
+            Console.WriteLine(doc.PrettyPrint());
+            return null;
+        }
+    }
+}
+public class WikidataResultItem
+{
+    public string item { get; set; }
+    public string itemLabel { get; set; }
+    public string coords { get; set; }
+    public string pop { get; set; }
+    public (string name, double latitude, double longitude) ToData()
+    {
+        string[] split = coords.Replace("Point(","").Replace(")","").Split(" ");
+        return (itemLabel, double.Parse(split[0]), double.Parse(split[1]));
     }
 }
