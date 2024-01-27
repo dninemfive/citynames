e414f1900cc63348debebc71af8171497d33358d
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-01-27 13:35:50 -0800

    abortive attempt at making a generic markov process class

---
+++ IMarkovProcess.cs
@@ -0,0 +1,32 @@
+﻿using d9.utl;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace citynames;
+public interface IMarkovProcess<TContext, TElement>
+    where TContext : notnull, IEnumerable<TElement>
+    where TElement : notnull
+{
+    protected Dictionary<TContext, CountingDictionary<TElement, int>> Data { get; set; }
+    public TElement? NextElement(TContext? context = default);
+    public TContext Preprocess(TContext item) => item;
+    public void Add(TContext item)
+    {
+        if (Data is null)
+            Data = new();
+        Preprocess(item);
+        
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!Data.ContainsKey(cur))
+                Data[cur] = new();
+            // Console.WriteLine($"{"".PadLeft(i)}{cur}");
+            Data[cur].Increment(s[i]);
+            cur = s[i];
+        }
+    }
+}
