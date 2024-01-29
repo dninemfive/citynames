9cf670f9703f5f6f7f6e72cd9e2d7d414d78df35
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-01-29 15:03:43 -0800

    document WikidataResultItem

--- WikidataResultItem.cs
+++ WikidataResultItem.cs
@@ -1,16 +1,42 @@
-﻿namespace citynames;
+﻿using System.Text.Json.Serialization;
+
+namespace citynames;
+/// <summary>
+/// Wrapper for Wikidata, uh, data to make (de)serializing it cleaner.
+/// </summary>
 public class WikidataResultItem
 {
-    public string item { get; set; }
-    public string itemLabel { get; set; }
     /// <summary>
-    /// Ordered (longitude, latitude), i.e. (x, y)
+    /// The city's Wikidata identifier.
     /// </summary>
-    public string coords { get; set; }
-    public string pop { get; set; }
-    public (string name, LatLongPair coords) ToData()
+    [JsonPropertyName("item")]
+    public required string Item { get; set; }
+    /// <summary>
+    /// The city's Wikidata article name.
+    /// </summary>
+    [JsonPropertyName("itemLabel")]
+    public required string Name { get; set; }
+    /// <summary>
+    /// The city's geographic coordinates, interpreted according to the GPS (wkid 4326) 
+    /// <see href="https://developers.arcgis.com/documentation/spatial-references/">spatial reference</see>.
+    /// </summary>
+    /// <remarks>Note: Coordinates are ordered (longitude, latitude), i.e. (x, y), by Wikidata, so these are 
+    /// reversed to fit the latitude-longitude ordering used by ArcGIS.</remarks>
+    [JsonPropertyName("coords")]
+    public required string Coordinates { get; set; }
+    /// <summary>
+    /// The population of the city in question.
+    /// </summary>
+    [JsonPropertyName("pop")]
+    public required string Population { get; set; }
+    /// <summary>
+    /// Converts the string describing the city's <see cref="Coordinates"/> to a <see cref="LatLongPair"/> and returns it with the name.
+    /// </summary>
+    /// <returns>The city's <see cref="Name"/> and a <see cref="LatLongPair"/> describing its coordinates.</returns>
+    /// <remarks><inheritdoc cref="Coordinates" path="/remarks"/></remarks>
+    public (string name, LatLongPair coordinates) ToData()
     {
-        string[] split = coords.Replace("Point(","").Replace(")","").Split(" ");
-        return (itemLabel, new(double.Parse(split[1]), double.Parse(split[0])));
+        string[] split = Coordinates.Replace("Point(","").Replace(")","").Split(" ");
+        return (Name, new(double.Parse(split[1]), double.Parse(split[0])));
     }
 }
