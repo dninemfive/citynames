bcb86d6bea281831507ad37c712a727ec82c7b9a
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-01-29 13:03:40 -0800

    context length command line arg

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
 ﻿using citynames;
 using d9.utl;
 using System.Text.Json;
-
+namespace citynames;
 public class Program
 {
-    public const string GENERATOR_FILENAME = "generators.json";
+    public static string GeneratorFilename { get; private set; } = "";
     public const string OUTPUT_DIRECTORY = "output";
     private static async Task Main()
     {
-        bool generate = CommandLineArgs.GetFlag("regenerate") || !File.Exists(GENERATOR_FILENAME);
-        int numPerBiome = CommandLineArgs.TryParseValue<int>("numPerBiome") ?? 10;
+        int numPerBiome = CommandLineArgs.TryParseValue<int>("numPerBiome") ?? 10,
+            contextLength = CommandLineArgs.TryParseValue<int>("contextLength") ?? 2;
+        GeneratorFilename = $"generators_{contextLength}.json";
+        bool generate = CommandLineArgs.GetFlag("regenerate") || !File.Exists(GeneratorFilename);
         Dictionary<string, MarkovStringGenerator> generatorsByBiome;
         if(generate)
         {
@@ -18,7 +20,7 @@ public class Program
             {
                 if (!generatorsByBiome.TryGetValue(biome, out MarkovStringGenerator? generator))
                 {
-                    generator = new(contextLength: 2);
+                    generator = new(contextLength);
                     generatorsByBiome[biome] = generator;
                 }
                 generator.Add(city.Split("(")[0].Split(",")[0]);
@@ -26,14 +28,14 @@ public class Program
         } 
         else
         {
-            generatorsByBiome = JsonSerializer.Deserialize<Dictionary<string, MarkovStringGenerator>>(File.ReadAllText(GENERATOR_FILENAME))!;
+            generatorsByBiome = JsonSerializer.Deserialize<Dictionary<string, MarkovStringGenerator>>(File.ReadAllText(GeneratorFilename))!;
         }
         Console.WriteLine("Data loaded.");
         Querier.SaveCache();
         if(generate)
         {
             Console.Write($"Saving generators...");
-            File.WriteAllText(GENERATOR_FILENAME, JsonSerializer.Serialize(generatorsByBiome));
+            File.WriteAllText(GeneratorFilename, JsonSerializer.Serialize(generatorsByBiome));
             Console.WriteLine($"...Done!");
         }
         Console.WriteLine("Generating city names...");
