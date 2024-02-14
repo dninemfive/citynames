e727185161693431f102907b6be5c09d19fde3fb
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-02-14 11:56:35 -0800

    sketch RegressionStringGenerator

---
+++ RegressionStringGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace citynames;
+public class RegressionStringGenerator : IStringGenerator
+{
+}
+public class ContextCharDataItem
+{
+    public string Context;
+    public char Result;
+    public OneHotCategoryEncoding Biome;
+    public float DistanceFromCoast;
+}
+public class OneHotCategoryEncoding
+{
+
+}
