cba404f2510c9815825c5709357e8ec96a314b61
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-02-18 14:48:56 -0800

    forgot to actually add the new abstractions

---
+++ String Generation/IBuildLoadAbleStringGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace citynames;
+public interface IBuildLoadAbleStringGenerator<T, TSelf> : ISaveableStringGenerator<T>
+{
+    public static abstract Task<TSelf> BuildAsync(IAsyncEnumerable<T> input, int contextLength = 2);
+    public static abstract Task<TSelf> LoadAsync(string path);
+}
