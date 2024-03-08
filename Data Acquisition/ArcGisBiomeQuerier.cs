d8440f8e2beb3dfa3d7539cb74eeaa52397d17b4
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-03-08 11:32:21 -0800

    ArcGisBiomeQuerier

---
+++ Data Acquisition/ArcGisBiomeQuerier.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace citynames;
+internal class ArcGisBiomeQuerier(HttpClient _client)
+{
+    private readonly Cache<LatLongPair, string> _cache = new("biomeCache.json");
+    internal static string BaseUrl { get; private set; } = File.ReadAllText(@"C:\Users\dninemfive\Documents\workspaces\misc\citynames\arcgis query url.txt");
+    private async Task<JsonDocument?> QueryApiFor(LatLongPair coords)
+        => await _client.GetFromJsonAsync<JsonDocument>(BaseUrl.Replace("{y}", $"{coords.Latitude}")
+                                                               .Replace("{x}", $"{coords.Longitude}"));
+    private static string? BiomeFromQuery(JsonDocument? doc)
+        => doc?.RootElement.NullablyGetProperty("layers")?
+                           .FirstArrayElement()?
+                           .NullablyGetProperty("features")?
+                           .FirstArrayElement()?
+                           .NullablyGetProperty("attributes")?
+                           .NullablyGetProperty("BIOME_NAME")?
+                           .GetString();
+    public async Task<(string? biome, bool cacheHit)> TransformAsync(LatLongPair coords)
+    {
+        _cache.EnsureLoaded();
+        if (_cache!.TryGetValue(coords, out string? biome))
+            return (biome, true);
+        biome = BiomeFromQuery(await QueryApiFor(coords));
+        if(biome is not null)
+            _cache[coords] = biome!;
+        return (biome, false);
+    }
+}
---
+++ Utils/JsonUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace citynames;
+internal static class JsonUtils
+{
+    internal static JsonElement? NullablyGetProperty(this JsonElement el, string propertyName)
+        => el.TryGetProperty(propertyName, out JsonElement result) ? result : null;
+    internal static JsonElement? FirstArrayElement(this JsonElement el)
+    {
+        try
+        {
+            return el.EnumerateArray().First();
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
