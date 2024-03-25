7419e0094593fe68a3d1b42e39394479e01d9200
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-03-25 13:19:05 -0700

    new approach!

---
+++ String Generation/MarginalProbabilityStringGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace citynames;
+internal class MarginalProbabilityStringGenerator : IBuildLoadableStringGenerator<CharPair, MarginalProbabilityStringGenerator>
+{
+}
+public class CharPair
+{
+    public char Ancestor, Successor;
+    // e.g. -1 = {ancestor}{successor}, -2 = {ancestor}.{successor}, -3 = {ancestor}..{successor}
+    public int Offset;
+    public QueryInfo Data;
+}
+public class QueryInfo
+{
+    public Dictionary<string, float> BiomeWeights;
+}
