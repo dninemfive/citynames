8357e0188ab667e70287c1d1119d82cff4429d9d
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-03-27 14:06:43 -0700

    reflection documentation

--- Reflection/GeneratorAttribute.cs
+++ Reflection/GeneratorAttribute.cs
@@ -1,14 +1,15 @@
 ﻿namespace citynames;
+/// <summary>
+/// An attribute which marks the specified type as a potential option for string generation.
+/// </summary>
+/// <param name="name">The name used to select the type on the command line.</param>
+/// <param name="fileName">The base name used to save and load the respective string generator.</param>
 [AttributeUsage(AttributeTargets.Class)]
-public class GeneratorAttribute : Attribute
+public class GeneratorAttribute(string name, string fileName) : Attribute
 {
-    public string Name { get; private set; }
-    public string BaseFileName { get; private set; }
-    public GeneratorAttribute(string name, string fileName)
-    {
-        Name = name;
-        BaseFileName = fileName;
-    }
+    public string Name { get; private set; } = name;
+    public string BaseFileName { get; private set; } = fileName;
+
     public string FileNameFor(int contextLength)
         => BaseFileName.Replace("{contextLength}", $"{contextLength}");
 }
--- Reflection/GeneratorInfo.cs
+++ Reflection/GeneratorInfo.cs
@@ -3,9 +3,19 @@ using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
 namespace citynames;
+/// <summary>
+/// Defines and automates instantiation of specific string generators marked with 
+/// <see cref="GeneratorAttribute"/>.
+/// </summary>
 public class GeneratorInfo
 {
-    public readonly string Name, BaseFileName;
+    /// <summary><inheritdoc cref="GeneratorAttribute" path="/param[@name='name']/node()"/></summary>
+    public string Name { get; private set; }
+    /// <summary><inheritdoc cref="GeneratorAttribute" path="/param[@name='fileName']/node()"/></summary>
+    public string BaseFileName { get; private set; }
+    /// <summary>
+    /// The type of the associated string generator.
+    /// </summary>
     public readonly Type Type;
     private GeneratorInfo(Type type, GeneratorAttribute attr)
     {
@@ -13,16 +23,46 @@ public class GeneratorInfo
         Name = attr.Name;
         BaseFileName = attr.BaseFileName;
     }
+    /// <summary>
+    /// Gets the <see cref="GeneratorInfo"/> for a specified <paramref name="type"/>, if any.
+    /// </summary>
+    /// <param name="type">The type whose GeneratorInfo to produce.</param>
+    /// <returns>The produced GeneratorInfo.</returns>
+    /// <remarks>Because this is <see langword="private"/>, it is assumed that the
+    ///          <paramref name="type"/> actually has an associated GeneratorAttribute;
+    ///          an exception will be thrown if used improperly.</remarks>
     private static GeneratorInfo FromType(Type type)
         => new(type, type.GetCustomAttribute<GeneratorAttribute>()!);
+    /// <summary>
+    /// The file name for this type of generator with the given parameters.
+    /// </summary>
+    /// <param name="contextLength">The context length used to produce this particular generator instance.</param>
+    /// <returns>The file name to save this generator to or load the same from.</returns>
     public string FileNameFor(int contextLength)
         => BaseFileName.Replace("{contextLength}", $"{contextLength}");
     private static readonly Dictionary<string, GeneratorInfo> _dict
             = ReflectionUtils.AllLoadedTypesWithAttribute<GeneratorAttribute>()
                              .Select(FromType)
                              .ToDictionary(x => x.Name);
+    /// <summary>
+    /// Tries to get the <see cref="GeneratorInfo"/> associated with the specified 
+    /// <see cref="Name">Name</see>, if it exists.
+    /// </summary>
+    /// <param name="name">The name of the GeneratorInfo to get.</param>
+    /// <param name="info">The GeneratorInfo, if found, or <see langword="null"/> otherwise.</param>
+    /// <returns><see langword="true"/> if the info was found, or <see langword="false"/> otherwise.
+    /// </returns>
     public static bool TryGetByName(string name, [NotNullWhen(true)] out GeneratorInfo? info)
         => _dict.TryGetValue(name, out info);
+    /// <summary>
+    /// Gets the <see cref="GeneratorInfo"/> associated with the specified <see cref="Name">Name</see>,
+    /// throwing an exception if not found.
+    /// </summary>
+    /// <param name="name"><inheritdoc
+    ///                         cref="TryGetByName(string, out GeneratorInfo?)"
+    ///                         path="/param[@name='name']/node()"/>
+    /// </param>
+    /// <returns>The <see cref="GeneratorInfo"/> with the specified <paramref name="name"/>.</returns>
     public static GeneratorInfo GetByName(string name)
         => TryGetByName(name, out GeneratorInfo? result) ? result : throw InvalidGeneratorTypeException(name);
     private static ArgumentException InvalidGeneratorTypeException(string name)
@@ -56,6 +96,16 @@ public class GeneratorInfo
         action.InvokeWithMessage($"{methodName}ing {Type.Name} with args {args.Select(x => x.ShortString()).ListNotation()}");
         return result;
     }
+    /// <summary>
+    /// Tries to build or load a specified string generator.
+    /// </summary>
+    /// <param name="contextLength">The context length of the specific generator to build.</param>
+    /// <param name="ngramFn">A function which, when called, will provide the required ngrams to build the generator.</param>
+    /// <param name="forceRebuild">If <see langword="true"/>, loading will not be attempted and the
+    ///        generator will always be built.</param>
+    /// <returns>The generator as defined above.</returns>
+    /// <exception cref="ArgumentException">Thrown if both loading and building fail, usually because
+    ///            the type does not implement the required methods.</exception>
     public async Task<ISaveableStringGenerator<NgramInfo>> Instantiate(int contextLength, Func<IEnumerable<NgramInfo>> ngramFn, bool forceRebuild = false)
     {
         object? obj = null;
