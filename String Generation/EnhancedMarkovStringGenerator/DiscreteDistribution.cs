75d70336afbc9bb7daeffd4d9ecf9fc840e796a3
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-03-29 10:43:16 -0700

    split IDistribution and DiscreteDistribution files and document them

---
+++ String Generation/EnhancedMarkovStringGenerator/DiscreteDistribution.cs
@@ -0,0 +1,65 @@
+﻿using d9.utl;
+using System.Collections;
+using System.Numerics;
+
+namespace citynames;
+/// <summary>
+/// Models a distribution across discrete variables.
+/// </summary>
+/// <typeparam name="K"><inheritdoc cref="IDistribution{K, V}" path="/typeparam[@name='K']"/></typeparam>
+/// <typeparam name="V"><inheritdoc cref="IDistribution{K, V}" path="/typeparam[@name='V']"/></typeparam>
+/// <param name="dict">A dictionary containing data for this distribution. If <see langword="null"/>,
+///                    an empty distribution is initialized.</param>
+public class DiscreteDistribution<K, V>(CountingDictionary<K, V>? dict = null)
+    : IDistribution<K, V>, IEnumerable<KeyValuePair<K, V>>
+    where K : notnull
+    where V : struct, IFloatingPoint<V>
+{
+    private readonly CountingDictionary<K, V> _dict = dict ?? new();
+    /// <summary>
+    /// The total weight of data points in this distribution.
+    /// </summary>
+    public V Weight { get; private set; } = dict?.Values.Sum() ?? V.Zero;
+    /// <summary>
+    /// Adds an item to this distribution.
+    /// </summary>
+    /// <param name="key">The key to which to add.</param>
+    /// <param name="value">The value to add to the specified key. If <see langword="null"/>, defaults to 1.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if the value added is negative.</exception>
+    public void Add(K key, V? value = null)
+    {
+        if (value is not null && value <= V.Zero)
+            throw new ArgumentOutOfRangeException(nameof(value));
+        Weight += value ?? V.One;
+        _dict[key] += value ?? V.One;
+    }
+    public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
+        => ((IEnumerable<KeyValuePair<K, V>>)_dict).GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator()
+        => ((IEnumerable)_dict).GetEnumerator();
+    /// <summary>
+    /// Gets the probability that the specified <paramref name="key"/> would occur if this distribution were randomly sampled. 
+    /// </summary>
+    /// <param name="key">The key whose probability to get.</param>
+    /// <returns>The probability that the key occurs. As a probability, it is normalized to the range [0..1].</returns>
+    public V this[K key]
+        => _dict.TryGetValue(key, out V value) ? value / Weight : V.Zero;
+    /// <summary>
+    /// Multiplies the values in this distribution by a constant factor.
+    /// </summary>
+    /// <param name="distribution">The distribution to multiply.</param>
+    /// <param name="factor">The factor by which to multiply the variables.</param>
+    /// <returns>A distribution corresponding to the input distribution with each variable multiplied by the specified factor.</returns>
+    public static DiscreteDistribution<K, V> operator *(DiscreteDistribution<K, V> distribution, V factor)
+        => new(distribution._dict * factor);
+    /// <summary>
+    /// Adds two distributions together.
+    /// </summary>
+    /// <param name="a">The first distribution to add.</param>
+    /// <param name="b">The second distribution to add.</param>
+    /// <returns>A new distribution where each variable has been added pairwise. If there is not a
+    ///          corresponding variable in one or the other distribution, the variable will be included
+    ///          unchanged, i.e. the set of variables is the union of the input variable sets.</returns>
+    public static DiscreteDistribution<K, V> operator +(DiscreteDistribution<K, V> a, DiscreteDistribution<K, V> b)
+        => new(a._dict + b._dict);
+}
---
+++ String Generation/EnhancedMarkovStringGenerator/IDistribution.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+namespace citynames;
+/// <summary>
+/// Abstraction for mapping keys to floating-point values.
+/// </summary>
+/// <typeparam name="K">The type of the input variable for this distribution.</typeparam>
+/// <typeparam name="V">The type of the output variable for this distribution.</typeparam>
+public interface IDistribution<K, V>
+    where K : notnull
+    where V : IFloatingPoint<V>
+{
+    public V this[K key] { get; }
+}
