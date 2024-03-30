cd4a017fba9a0faaa0ed784d5512808f2323265c
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-03-30 11:35:01 -0700

    delete unused DataProcessor and associated tests; merge .NgramInfos and .ToNgramsInternal

--- String Generation/NgramInfoExtensions.cs
+++ String Generation/NgramInfoExtensions.cs
@@ -1,35 +1,23 @@
 ﻿namespace citynames;
 public static class NgramInfoExtensions
 {
-    public static IEnumerable<NgramInfo> NgramInfos(this string cityName, string biome, int contextLength = 2)
+    public static IEnumerable<NgramInfo> NgramInfos(this string cityName, string biome, int contextLength = 2, string breakChars = ",(")
     {
         cityName = cityName.AppendIfNotPresent(Characters.STOP);
         string cur = "";
         for (int i = 1 - contextLength; i <= cityName.Length - contextLength; i++)
         {
-            yield return new(cur, $"{cityName[i + contextLength - 1]}", biome);
-            cur = cityName.SubstringSafe(i, i + contextLength);
-        }
-    }
-    private static IEnumerable<NgramInfo> ToNgramsInternal(this string cityName, string biome, int contextLength = 2, string breakChars = ",(")
-    {
-        foreach (NgramInfo ngram in cityName.NgramInfos(biome, contextLength))
-        {
-            if (breakChars.Contains(ngram.Successor))
+            string successor = $"{cityName[i + contextLength - 1]}";
+            if (breakChars.Contains(successor))
                 break;
-            yield return ngram;
+            yield return new(cur, successor, biome);
+            cur = cityName.SubstringSafe(i, i + contextLength);
         }
     }
     public static IEnumerable<NgramInfo> ToNgrams(this IEnumerable<(string cityName, string biome)> rawData, int contextLength = 2, string breakChars = ",(")
     {
         foreach ((string cityName, string biome) in rawData)
-            foreach (NgramInfo ngram in cityName.ToNgramsInternal(biome, contextLength, breakChars))
-                yield return ngram;
-    }
-    public static async IAsyncEnumerable<NgramInfo> ToNgramsAsync(this IAsyncEnumerable<(string cityName, string biome)> rawData, int contextLength = 2, string breakChars = ",(")
-    {
-        await foreach ((string cityName, string biome) in rawData)
-            foreach (NgramInfo ngram in cityName.ToNgramsInternal(biome, contextLength, breakChars))
+            foreach (NgramInfo ngram in cityName.NgramInfos(biome, contextLength, breakChars))
                 yield return ngram;
     }
 }
