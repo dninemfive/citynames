67fe2a0436d680d94eb545ac4d90d778cb7ced9b
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-03-30 13:07:12 -0700

    document ISaveableStringGenerator and IBuildLoadableStringGenerator

--- String Generation/IBuildLoadAbleStringGenerator.cs
+++ String Generation/IBuildLoadAbleStringGenerator.cs
@@ -1,6 +1,26 @@
 ﻿namespace citynames;
+/// <summary>
+/// Represents a string generator which can be built from data and loaded from a saved file.
+/// </summary>
+/// <typeparam name="T"><inheritdoc cref="ISaveableStringGenerator{T}" path="/typeparam[@name='T']/node()"/></typeparam>
+/// <typeparam name="TSelf">The type of the string generator itself.</typeparam>
 public interface IBuildLoadableStringGenerator<T, TSelf> : ISaveableStringGenerator<T>
 {
+    /// <summary>
+    /// Builds the string generator from a specified <paramref name="corpus"/> of strings and 
+    /// associated metadata, given a specific <paramref name="contextLength"/>.
+    /// </summary>
+    /// <param name="corpus">
+    ///     A collection of strings paired with metadata which should provide associations which
+    ///     allow the string generator to vary its output in response to queries.
+    /// </param>
+    /// <param name="contextLength">The length of context n-grams.</param>
+    /// <returns>A model built as described above.</returns>
     public static abstract TSelf Build(IEnumerable<(string item, T metadata)> corpus, int contextLength = 2);
+    /// <summary>
+    /// Loads the string generator from a saved file.
+    /// </summary>
+    /// <param name="path">The path to the file from which to load the string generator.</param>
+    /// <returns>The loaded string generator.</returns>
     public static abstract TSelf Load(string path);
 }
--- String Generation/ISaveableStringGenerator.cs
+++ String Generation/ISaveableStringGenerator.cs
@@ -1,6 +1,25 @@
 ﻿namespace citynames;
+/// <summary>
+/// Represents a class which can generate a random string and save itself.
+/// </summary>
+/// <typeparam name="T">A type representing the metadata which provides specifications to which the
+///                     model should conform when generating strings.</typeparam>
 public interface ISaveableStringGenerator<T>
 {
-    public string RandomString(T input, int minLength, int maxLength);
+    /// <summary>
+    /// Generates a random string between <paramref name="minLength"/> and <paramref name="maxLength"/>,
+    /// subject to specifics given by <paramref name="query"/>.
+    /// </summary>
+    /// <param name="query">Metadata to be used by the string generator to reflect in its output.</param>
+    /// <param name="minLength">The minimum length, <b>inclusive</b>, of the resulting string.</param>
+    /// <param name="maxLength">The maximum length, <b>inclusive</b>, of the resulting string.</param>
+    /// <returns>A random string as described above.</returns>
+    public string RandomString(T query, int minLength, int maxLength);
+    /// <summary>
+    /// Saves the string generator to the specified <paramref name="path"/> so that it can (ideally)
+    /// be loaded later.
+    /// </summary>
+    /// <param name="path">The path to which to save the string generator.</param>
+    /// <returns><see cref="Task.CompletedTask"/> when saving is complete.</returns>
     public Task SaveAsync(string path);
 }
