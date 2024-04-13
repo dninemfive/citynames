06d871d457b4106bfddd5acc825477b2f28d934c
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-04-13 13:33:24 -0700

    what am i even doing anymore

--- Utils/Types/VectorEncoding.cs
+++ Utils/Types/VectorEncoding.cs
@@ -1,5 +1,6 @@
 ﻿using MathNet.Numerics;
 using System.Numerics;
+using System.Text;
 using System.Text.Json.Serialization;
 
 namespace citynames;
@@ -8,22 +9,25 @@ public class VectorEncoding<T, U>
     where U : struct, IFloatingPoint<U>
 {
     [JsonInclude]
+    public T? Default { get; private set; }
+    [JsonInclude]
     public IReadOnlyList<T> Alphabet { get; private set; }
     [JsonIgnore]
     private readonly Dictionary<T, U[]> _encodingCache = new();
     [JsonConstructor]
-    private VectorEncoding(IReadOnlyList<T>  alphabet)
+    private VectorEncoding(IReadOnlyList<T> alphabet, T? @default = default)
     {
         Alphabet = alphabet;
+        Default = @default;
     }
-    public static VectorEncoding<T, U> From(IEnumerable<T> items)
-        => new([.. items.Distinct().Order()]);
-    public U[] Encode(T item)
+    public static VectorEncoding<T, U> From(IEnumerable<T> items, T? @default = default)
+        => new([.. items.Distinct().Where(x => !x.Equals(@default)).Order()], @default);
+    public U[] Encode(T item, bool throwIfNotInAlphabet = false)
     {
         if (_encodingCache.TryGetValue(item, out U[]? cachedResult))
             return cachedResult;
         //Console.WriteLine(LogUtils.MethodArguments(arguments: [(nameof(item), item)]));
-        if (!Alphabet.Contains(item))
+        if (throwIfNotInAlphabet && !Alphabet.Contains(item))
             throw new ArgumentOutOfRangeException(nameof(item));
         U[] result = new U[DimensionCount];
         for (int i = 0; i < result.Length; i++)
@@ -42,14 +46,28 @@ public class VectorEncoding<T, U>
     {
         if (encoded.Length != Alphabet.Count)
             throw new ArgumentException($"Encoded vector has {encoded.Length} dimensions, but this encoding requires {DimensionCount}!", nameof(encoded));
-        if (encoded.Any(x => x is not (0 or 1)) || encoded.Count(x => x == U.One) != 1)
-            throw new ArgumentException($"OneHotEncoding.{nameof(DecodeOneHot)} can only decode vectors with exactly one 1-valued item; all others must be 0!", nameof(encoded));
+        if(encoded.All(U.IsZero))
+            return Default ?? throw DefaultIsNull(nameof(encoded));
+        if (encoded.Any(x => x != U.Zero || x != U.One))
+            throw NotOneHot(nameof(encoded));
+        if (encoded.Count(x => x == U.One) != 1)
+            throw MoreThanOneHotValue(nameof(encoded));
         return Alphabet[encoded.IndexOfMaxValue()];
     }
+    #region exceptions
+    private ArgumentException DimensionMismatch(string name, int length)
+        => new($"Encoded vector has {length} dimensions, but this encoding requires {DimensionCount}!", name);
+    private ArgumentException DefaultIsNull(string name)
+        => new($"Attempted to decode an all-zero array, but {this} does not have a default value!", name);
+    private static ArgumentException NotOneHot(string name)
+        => new($"Attempted to decode an array as one-hot, but it had values which are not zero or one!", name);
+    private static ArgumentException MoreThanOneHotValue(string name)
+        => new($"Attempted to decode an array as one-hot, but it had multiple hot values!", name);
+    #endregion exceptions
     public IEnumerable<T> Decode(U[] encoded, U? threshold = null)
     {
         if (encoded.Length != Alphabet.Count)
-            throw new ArgumentException($"Encoded vector has {encoded.Length} dimensions, but this encoding requires {DimensionCount}!", nameof(encoded));
+            throw DimensionMismatch(nameof(encoded), encoded.Length);
         threshold ??= encoded.Max();
         for(int i = 0; i < encoded.Length; i++)
         {
